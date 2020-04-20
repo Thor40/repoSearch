ef36653a49544c609a8fe2492b27c9ffcfad190a
Thor40 <nicholas.kosik@gmail.com>
2020-04-20 13:59:16 -0500

    fetch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,18 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    //format the gitbuh api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
+    // fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    //     response.json().then(function(data) {
+    //         console.log(data);
+    //     });
+    // });
   };
 
 getUserRepos();
