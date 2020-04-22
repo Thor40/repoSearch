a2a3626880bd40c1ff6c139a4acd37c4e3efefed
Thor40 <nicholas.kosik@gmail.com>
2020-04-22 12:14:27 -0500

    ref both html

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,6 +8,9 @@ var getRepoIssues = function(repo) {
         if (response.ok){
      response.json().then(function(data) {
          displayIssues(data);
+         if (response.headers.get("Link")) {
+             displayWarning(repo);
+         }
         });
     } 
     else {
@@ -50,4 +54,18 @@ var displayIssues = function(issues) {
     issueContainerEl.appendChild(issueEl);
 };
 
-getRepoIssues("facebook/react");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit: ";
+
+    //append link with href
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl. setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
+
+getRepoIssues("expressjs/express");
